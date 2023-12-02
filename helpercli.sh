a7851a48dcd829f9d43506fe8ab2f481c5c67c2c
levkins <levkins@bk.ru>
2023-12-02 13:50:55 +0200

    fix start script

--- helpercli.sh
+++ helpercli.sh
@@ -4,7 +4,8 @@ folder=$PWD   #~/shells/
 export "DB_FILE=$folder/helpercli.db"
 randomfile=$(cat /dev/urandom | tr -dc A-Za-z0-9 | head -c 6)
 mkdir /tmp/helpercli/
-sqlite3 $DB_FILE "SELECT txt FROM node WHERE name='cli'" > /tmp/helpercli/$randomfile.bash
+sqlite3 $DB_FILE "SELECT txt FROM node WHERE name='cli'" > /tmp/helpercli/$randomfile.sh
+ls -ls /tmp/helpercli/
 bash /tmp/helpercli/$randomfile.sh $1 $2
-#echo "Start DELETE FILES"
+echo "Start DELETE FILES"
 rm -rf /tmp/helpercli/
